2e4364a1b5062ca63bf930c15fd8864195bd8bf1
pamelagoncalves <pamelagoncalves@users.noreply.github.com>
2020-04-13 01:57:45 -0300

    projeto finalizado

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,60 @@
 const cipher = {
-  // ...
-};
+
+  // CIFRAR
+  encode: function cipher(offset, mensagem) {
+    let deslocamento = Number(offset);
+    // essas var é uma declaração de vars que estao fora do laço
+    let msgCode = [];
+    // Vareavel em array recebe mais de uma informação dentro dela.
+    if ((mensagem !== 0 && mensagem !== null) && (offset !== 0 && offset !== null)) {
+      for (let contador = 0; contador < mensagem.length; contador++) {
+        let codigoDaLetraAsc = mensagem.toUpperCase().charCodeAt(contador);
+        if (codigoDaLetraAsc !== 32) {
+          //basicamente a formula matematica que devolve a nova letra
+          let novaLetra = String.fromCharCode(((codigoDaLetraAsc - 65 + deslocamento) % 26) + 65);
+
+          // Pega os elementos e adiciona dentro da var vetor.
+          msgCode.push(novaLetra);
+        }
+        else {
+          msgCode.push(" ");
+        }
+      }
+    }
+    else {
+      throw new TypeError;
+    }
+    // retorna o texto sem a virgula
+    return msgCode.join("");
+  },
+
+
+
+  // DECIFRAR 
+  decode: function decipher(offset, mensagem) {
+    let deslocamento = Number(offset);
+    let msgDecode = [];
+    if ((mensagem !== 0 && mensagem !== null) && (offset !== 0 && offset !== null)) {
+      for (let contador = 0; contador < mensagem.length; contador++) {
+
+        // cod letra
+        let codigoDaLetraAsc = mensagem.toUpperCase().charCodeAt(contador);
+        if (codigoDaLetraAsc !== 32) {
+          let novaLetra = String.fromCharCode(((codigoDaLetraAsc + 65 - deslocamento) % 26) + 65);
+          msgDecode.push(novaLetra);
+        }
+        else {
+          msgDecode.push(" ");
+        }
+      }
+    }
+    else {
+      throw new TypeError;
+    }
+    // retorna o texto sem a virgula
+    return msgDecode.join("");
+  }
+}
+
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 import cipher from './cipher.js';
 
+let botaoCodificar = document.getElementById("codificar");
+let botaoDecodificar = document.getElementById("decodificar");
+botaoCodificar.addEventListener("click", printarResultadoCode);
+botaoDecodificar.addEventListener("click", printarResultadoDecode);
+
+function printarResultadoCode() {
+    let mensagem = document.getElementById("mensagem").value.toUpperCase();
+    let offset = document.getElementById("offset").value;
+    if (mensagem === "" || offset === "") {
+        alert("nenhuma mensagem ou numero digitado, Favor preencher")
+    }
+    else {
+        document.getElementById("resultado").innerHTML = `A PALAVRA CIFRADA É: ${cipher.encode(offset, mensagem)}`;
+    }
+}
+
+function printarResultadoDecode() {
+    let mensagem = document.getElementById("mensagem").value.toUpperCase();
+    let offset = document.getElementById("offset").value;
+    if (mensagem === "" || offset === "") {
+        alert("nenhuma mensagem ou numero digitado, Favor preencher")
+    }
+    else {
+        document.getElementById("resultado").innerHTML = `A PALAVRA DECIFRADA É: ${cipher.decode(offset, mensagem)}`;
+    }
+
+}
+
 console.log(cipher);
